9e6d7edaea7f3b740f649cfbb28709c753dac7ff
mintchoco5555 <rossarin.m@kkumail.com>
2025-09-01 11:29:38 +0700

    ปรับแก้สคริปใน level1, level 2 บางส่วน

--- 2d-platformer-starter-kit-df8d514357fa28089188a4e2097e179a52bdf8a4/Scenes/Prefabs/level_finish_door_2.gd
+++ 2d-platformer-starter-kit-df8d514357fa28089188a4e2097e179a52bdf8a4/Scenes/Prefabs/level_finish_door_2.gd
@@ -5,4 +5,4 @@ func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		get_tree().call_group("Player", "death_tween") # death_tween is called here just to give the feeling of player entering the door.
 		#AudioManager.level_complete_sfx.play()
-		get_tree().change_scene_to_file("res://Scenes/Levels/menu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Managers/gameMenu.tscn")
--- 2d-platformer-starter-kit-df8d514357fa28089188a4e2097e179a52bdf8a4/Scripts/player.gd
+++ 2d-platformer-starter-kit-df8d514357fa28089188a4e2097e179a52bdf8a4/Scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 @export_category("Player Properties")
 @export var move_speed : float = 300
-@export var jump_force : float = 1000
+@export var jump_force : float = 500
 @export var gravity : float = 1200
 @export var max_jump_count : int = 2
 var jump_count : int = 2
